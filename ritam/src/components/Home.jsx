a0397d7b87d9903437e4b37e5e48b4ea69d61577
Ritam Majumder <ritmmajumder0@gmail.com>
2023-12-22 00:02:53 +0530

    components

---
+++ ritam/src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Achivements from './Achivements'
+import Academics from './Academics'
+import Skills from './Skills'
+import About from './About'
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      <Achivements/>
+      <Academics/>
+      <Skills/>
+      <About/>
+    </div>
+  )
+}
+
+export default Home
